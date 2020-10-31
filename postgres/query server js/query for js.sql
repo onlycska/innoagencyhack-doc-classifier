18ed3c8df983fa38b5bac6e99c94c99b479e40c2
NeinMaster4 <NeinMaster@mail.ru>
2020-10-31 23:50:51 +0300

    added insert in table image

--- postgres/query server js/query for js.sql
+++ postgres/query server js/query for js.sql
@@ -1,5 +1,10 @@
--- added user and return idusers
+-- add user and return idusers
 INSERT INTO postgres.public.users (username, userpass)
 VALUES
 ('name_user', 'pass_user')
 RETURNING idusers;
+
+--add image
+INSERT INTO postgres.public.image (url, recordid)
+VALUES
+('url', 'id');
